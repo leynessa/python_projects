1fb9b6d3387bc3a62d405dfc64e03e08e8608872
ashley williams <leynessa1@gmail.com>
2022-10-02 15:14:40 -0300

    band name generator

---
+++ day_one/band_name.py
@@ -0,0 +1,6 @@
+print("hey! Lets Get your Band Name")
+city_name = input("what city did you grow up in?")
+pet_name = input("what is the name of your pet? ")
+band_name = pet_name + "y" + " " + city_name
+print(band_name)
+print('Your  Band name  is' + " " + band_name)

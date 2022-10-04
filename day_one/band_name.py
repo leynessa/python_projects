a943bf362e1a8ddbe0d66164d72202babed2981f
ashley williams <leynessa1@gmail.com>
2022-10-03 23:57:26 -0300

    band name genrator

--- day_one/band_name.py
+++ day_one/band_name.py
@@ -3,4 +3,4 @@ city_name = input("what city did you grow up in?")
 pet_name = input("what is the name of your pet? ")
 band_name = pet_name + "y" + " " + city_name
 print(band_name)
-print('Your  Band name  is' + " " + band_name)
+print('Your wonderful Band name  is' + " " + band_name)
